2f38c6964ec538f08ebd3a0a899447630255aaea
José Alvaro <jose.alvaro.sf@hotmail.com>
2021-06-01 08:20:51 -0300

    SOFTDELE ADD

--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pessoa extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'pessoas';
     public $timestamps = true;
