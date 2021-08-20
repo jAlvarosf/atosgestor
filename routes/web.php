efad14ce019eb92635b9ec638741a9e753ebce74
José Alvaro <jose.alvaro.sf@hotmail.com>
2021-08-20 15:56:22 -0300

    configuração na rota para sempre só ter acesso a funções se estiver logado

--- routes/web.php
+++ routes/web.php
@@ -21,31 +21,31 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/painel', function () {
     return view('painel.index');
 })->name('Index');
 
-Route::get('/secretaria/secretaria.painel', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/secretaria/secretaria.painel', function () {
     return view('secretaria.painel');
 });
 
-Route::resource('secretaria', SecretariaController::class);
+Route::middleware(['auth:sanctum', 'verified'])->resource('secretaria', SecretariaController::class);
 
-Route::get('/secretaria/{$id}', [SecretariaController::class, 'edit']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/secretaria/{$id}', [SecretariaController::class, 'edit']);
 
-Route::get('/secretaria/{$id}', [SecretariaController::class, 'destroy']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/secretaria/{$id}', [SecretariaController::class, 'destroy']);
 
-Route::get('/financeiro/financeiro.painel', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/financeiro/financeiro.painel', function () {
     return view('financeiro.painel');
 });
 
-Route::get('financeiro/receitas/receitas.create', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('financeiro/receitas/receitas.create', function () {
     return view('financeiro/receitas.create');
 });
-Route::get('financeiro/receitas/receitas.index', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('financeiro/receitas/receitas.index', function () {
     return view('financeiro/receitas.index');
 });
 
-Route::get('financeiro/despesas/despesas.index', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('financeiro/despesas/despesas.index', function () {
     return view('financeiro/despesas.index');
 });
 
-Route::get('/financeiro/financeiro.extrato', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/financeiro/financeiro.extrato', function () {
     return view('financeiro.extrato');
 });
