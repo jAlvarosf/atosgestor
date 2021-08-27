ef4b27ad0033f26bddb799e4d00d5653e1ab1958
José Alvaro <jose.alvaro.sf@hotmail.com>
2021-08-27 11:23:51 -0300

    ok

--- resources/views/painel/index.blade.php
+++ resources/views/painel/index.blade.php
@@ -149,12 +149,12 @@
 @stop
 
 @section('css')
+<!-- Theme style -->
 <link rel="stylesheet" href="/css/admin_custom.css">
 
 @stop
 
 @section('js')
 <script>
-  console.log('Hi!');
 </script>
 @stop
